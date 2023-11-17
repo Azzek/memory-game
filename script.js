deb8e2ddab66e04e695c6a93e0da5627a4f4ee2c
Azzek <janchajecki2005@gmail.com>
2023-11-17 17:02:17 +0100

    #

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
+let wins = 0
 
 const cellsContainer = document.querySelector('.cells-container');
+
+document.querySelector('.ShowWin').classList.add('displayNone')
 
 let options = [
     {
     optionName:'Speedwagon',
-    Image:'',
+    Image:'resources/speedwagon.jpg',
     },{
     optionName:'Speedwagon',
-    Image:'',
+    Image:'resources/speedwagon.jpg',
     },{
     optionName:'Capibara',
-    Image:'',
+    Image:'resources/capibara.jpg',
     },{
     optionName:'Capibara',
-    Image:'',
-    },,{
-    optionName:'Kaczka',
-    Image:'',
+    Image:'resources/capibara.jpg',
     },{
-    optionName:'Kaczka',
-    Image:'',
+    optionName:'Sylphie',
+    Image:'resources/Syphiette.jpg',
     },{
-    optionName:'Asuna',
-    Image:'',
+    optionName:'Sylphie',
+    Image:'resources/Syphiette.jpg',
     },{
     optionName:'Asuna',
-    Image:'',
+    Image:'resources/asuna.jpg',
+    },{
+    optionName:'Asuna',
+    Image:'resources/asuna.jpg',
     },{
     optionName:'Shoko',
-    Image:'',
+    Image:'resources/Shoko.jpg',
     },{
     optionName:'Shoko',
-    Image:'',
+    Image:'resources/Shoko.jpg',
     },{
     optionName:'Yumeko',
-    Image:'',
+    Image:'resources/yumeko.jpg',
     },{
     optionName:'Yumeko',
-    Image:'',
+    Image:'resources/yumeko.jpg',
     },{
     optionName:'Hayase',
-    Image:'',
+    Image:'resources/hayase.jpg',
     },{
     optionName:'Hayase',
-    Image:'',
+    Image:'resources/hayase.jpg',
     },{
     optionName:'Mai',
-    Image:'',
+    Image:'resources/Mai.png',
     },{
     optionName:'Mai',
-    Image:'',
+    Image:'resources/Mai.png',
     },
 ]
           
@@ -59,12 +62,11 @@ let options = [
 
 randomizeOptions.forEach(option => {
     cellHTML +=`
-    <div class="cell js-cell-${option.optionName}" data-name=${option.optionName}><h1>${option.optionName}</h1></div>
+    <div class="cell js-cell-${option.optionName}" data-name=${option.optionName}><img src="${option.Image}"/></div>
     `
 });
 
 document.querySelector('.cells-container').innerHTML = cellHTML;
-
 
 const cells = document.querySelectorAll('.cell');
         
@@ -72,54 +74,91 @@ let showedCellsList = []
 
 let hitOptions = [];
 
+let gameRunning;
 cells.forEach((cell, i) => {
-    cell.classList.add('lol')
+
+    cell.classList.add('active')
+    cell.classList.add('non-pointer-events');
     cell.addEventListener('click', () =>{
 
         const name = cell.dataset.name
-        
+        cell.classList.add('non-pointer-events')
         showedCellsList.push(name)
-        cell.classList.remove('lol')
+        
+        cell.classList.remove('active')
+        
         if(showedCellsList.length === 2) {
             if(showedCellsList[0] !== showedCellsList[1]) {
-                showedCellsList.forEach((option) => {
-                 document.querySelectorAll(`.js-cell-${option}`).forEach((item) => {
-                 item.classList.add('lol')
+                cells.forEach((cell) => {
+                    cell.classList.add('non-pointer-events')
                 })
+                setTimeout(()=>{
+                    showedCellsList.forEach((option) => {
+                    document.querySelectorAll(`.js-cell-${option}`).forEach((item) => {
+                    item.classList.add('wrong')
+                    })})
+                },700)
+                setTimeout(() => {
+                showedCellsList.forEach((option) => {
+                document.querySelectorAll(`.js-cell-${option}`).forEach((item) => {
+                    item.classList.remove('wrong')
+                    item.classList.add('active')
+                })
+                })
+                cells.forEach((cell) => {
+                    cell.classList.remove('non-pointer-events')
                 })
                  showedCellsList = []
+                },1500)
             } else if(showedCellsList[0] === showedCellsList[1]){
-                console.log('lol')
+                console.log('active')
                 hitOptions.push(showedCellsList)
                 hitOptions.forEach((hitOption) => {
                 document.querySelectorAll(`.js-cell-${hitOption}`).forEach((item) => {
-                    item.classList.remove('lol')
-
+                    item.classList.remove('active')
+                    console.log('nice')
                 })
                 })
                 showedCellsList = [];
+                checkWin()
+                console.log(hitOptions.length)
             }
         }
     })
 })
 
-
-
-function checkHitDoube() {
-    for(let i = 0;  i < showedCellsList.length; i++) {
-        for(let j = i+1; j < showedCellsList.length; j++) {
-
-            if(showedCellsList[i] === showedCellsList[j]){
-
-                return true;
-            }
-        }
-    }
-    return false
-}
-
-
+document.querySelector('.startgameButton').addEventListener('click', ()=>{
+    startGame()
+})
+document.querySelector('.resetButton').addEventListener('click', ()=> {
+    location.reload()
+    gameRunning = false;
     
+})
+
+
+    function startGame() {
+        if(!gameRunning){
+            document.querySelectorAll('.cell').forEach((cell) => {
+            cell.classList.add('non-pointer-events');
+            cell.classList.remove('active')
+            setTimeout(() => {
+                cell.classList.add('active')
+                cell.classList.remove('non-pointer-events');
+            }, 2000);
+
+        })
+        gameRunning = true;
+    }
+    }
+
+    function checkWin(){
+        if(hitOptions.length === 8) {
+            document.querySelector('.ShowWin').classList.remove('displayNone')
+            wins++
+             saveWinsToLocalStorage ()
+        } 
+    }
         
 function shuffleArray(array) {
 
@@ -131,9 +170,9 @@ function shuffleArray(array) {
   }
     
 
-
-
-//
+  function saveWinsToLocalStorage () {
+    localStorage.setItem('Wins', wins)
+  }
 
 
 
